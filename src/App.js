21f140820451f2092048d0ac53229e4539cea9a7
ACER <210220@softwarica.edu.np>
2024-09-24 19:44:46 +0545

    sizeandresponsive

--- src/App.js
+++ src/App.js
@@ -7,10 +7,51 @@ const App = () => {
   return (
     <div>
       <div className="main-container w-full h-[100vh] flex">
+<Sidebar/>
 
-      <Sidebar/>
+    <div className="container h-full w-[85%] bg-red-700">
 
-    <div className="container h-full w-[85%] bg-red-700"></div>
+      {/* //Navbar */}
+      <div className="dashNav flex justify-between items-center bg-white mt-3">
+
+      <h1 className='font-mainfont font-bold text-lg md:text-2xl ml-3'>Dashboard</h1>
+
+      <div className="ACTIONS flex mx-5 gap-4">
+        <img src="../icons/Notification.png" className="cursor-pointer" alt="Notifications"/>
+        <img src="../icons/Avatar.png" className="cursor-pointer" alt="Avatar"/>
+        </div>
+      </div>
+
+      <div className="section-1 grid grid-cols-1 md:grid-cols-2 w-full h-full lg:h-[199px]">
+
+        {/* //Salesand Cost */}
+      <div className="SalesandCost w-[398px] h-[180px]  md:h-[199px] bg-white mt-3 flex ">
+      <img src="../images/business-and-finance.png" className=' bg-white  ml-2 mt-16 h-[40px] w-[40px] md:h-auto md:w-auto' alt="business-and-finance"/>
+      <div className="SaleCosts flex flex-col">
+        <h1 className=' text-xs font-bold text-black font-mainfont ml-5 mt-3'>Total Sales and Costs</h1>
+        <h2 className=' text-xs font-normal text-slate-500 font-mainfont ml-5'>Last 7 days</h2>
+        <h1 className=' text-md font-bold text-black font-mainfont ml-5 mt-5'>$350K</h1>
+        <h2 className=' text-xs font-normal text-slate-500 font-mainfont ml-5'><span className='text-green-600'>⬆️ 8.56K</span>   VS Last 7 Days</h2>
+        
+
+
+
+
+      </div>
+      </div>
+
+        {/* //TotalProfit */}
+      <div className="TotalProfit">
+
+      </div>
+
+
+
+      </div>
+
+
+
+    </div>
     </div>
     </div>
   )
--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -1,46 +1,46 @@
-import React from 'react'
+import React from 'react';
 
 const Sidebar = () => {
 return (
-<div className="sidebar h-full w-[15%] bg-white ">
+<div className="sidebar h-full w-[35%] md:w-[15%] bg-white ">
 
-<div className="Topbar mt-3 bg-white h-auto flex justify-start w-full">
-<img src="../images/logo.png" className="ml-2" alt="Logo"/>
-<h1 className='font-mainfont font-bold text-2xl ml-3'>Jobins</h1>
-<img src="../icons/indent-decrease.png" className="ml-20" alt="Logo"/>
+<div className="Topbar mt-3 bg-white h-auto flex justify-start items-start w-full">
+<img src="../images/logo.png" className="ml-2 h-[20px] w-[20px] md:h-auto md-w-auto" alt="Logo"/>
+<h1 className='font-mainfont font-bold text-sm md:text-2xl md:ml-3'>Jobins</h1>
+<img src="../icons/indent-decrease.png" className="cursor-pointer h-[20px] w-[20px] md:h-auto md:w-auto ml-5 xl:ml-10" alt="indent"/>
 </div>
 
 <div className="mainmenu mt-10 bg-white h-auto w-full flex flex-col justify-start">
-<h2 className='text-slate-500 font-mainfont font-semibold ml-3'>MAIN MENU</h2>
+<h2 className='text-slate-500 font-mainfont font-normal ml-3 text-xs md:text-lg'>MAIN MENU</h2>
 
-    <div className='DashRouter flex mt-3'>
-    <img src="../icons/smart-home.png" className="h-[20px] w-[20px] mx-3" alt="Logo"/>
-    <h1 className='text-slate-500 font-mainfont font-normal  cursor-pointer  hover:text-black hover:font-bold'>Dashboard</h1>
+    <div className='Dash-Router flex mt-3'>
+    <img src="../icons/smart-home.png" className="h-[20px] w-[20px] ml-3 md:mx-3" alt="Logo"/>
+    <h1 className='text-slate-500 font-mainfont font-normal  cursor-pointer text-xs md:text-lg hover:text-black hover:font-bold'>Dashboard</h1>
     </div>
 
-    <div className='DashRouter flex mt-3'>
-    <img src="../icons/shopping-cart.png" className="h-[20px] w-[20px] mx-3" alt="Logo"/>
-    <h1 className='text-slate-500 font-mainfont font-normal cursor-pointer  hover:text-black hover:font-bold'>Order Management</h1>
+    <div className='OrderManagement-Router flex mt-3'>
+    <img src="../icons/shopping-cart.png" className="h-[20px] w-[20px] ml-3 md:mx-3" alt="Logo"/>
+    <h1 className='text-slate-500 font-mainfont font-normal cursor-pointer text-xs md:text-lg hover:text-black hover:font-bold'>Order <br/>Management</h1>
     </div>
 
-    <div className='DashRouter flex mt-3'>
-    <img src="../icons/star.png" className="h-[20px] w-[20px] mx-3" alt="Logo"/>
-    <h1 className='text-slate-500 font-mainfont font-normal cursor-pointer  hover:text-black hover:font-bold'>Brand</h1>
+    <div className='Brand-Router flex mt-3'>
+    <img src="../icons/star.png" className="h-[20px] w-[20px] ml-3 md:mx-3" alt="Logo"/>
+    <h1 className='text-slate-500 font-mainfont font-normal cursor-pointer text-xs md:text-lg hover:text-black hover:font-bold'>Brand</h1>
     </div>
 
 </div>
 
-<div className="Products mt-10 bg-white h-auto w-full flex flex-col justify-start">
-<h2 className='text-slate-500 font-mainfont font-semibold ml-3'>PRODUCTS </h2>
+<div className="PRODUCTS mt-10 bg-white h-auto w-full flex flex-col justify-start">
+<h2 className='text-slate-500 font-mainfont font-normal text-xs md:text-lg ml-3 md:mx-3'>PRODUCTS </h2>
 
-    <div className='DashRouter flex mt-3'>
-    <img src="../icons/circle-plus.png" className="h-[20px] w-[20px] mx-3" alt="Logo"/>
-    <h1 className='text-slate-500 font-mainfont font-normal cursor-pointer  hover:text-black hover:font-bold'>Add Products</h1>
+    <div className='ADD-Router flex mt-3'>
+    <img src="../icons/circle-plus.png" className="h-[20px] w-[20px] ml-3 md:mx-3" alt="Logo"/>
+    <h1 className='text-slate-500 font-mainfont font-normal cursor-pointer text-xs md:text-lg hover:text-black hover:font-bold'>Add Products</h1>
     </div>
 
-    <div className='DashRouter flex mt-3'>
-    <img src="../icons/box.png" className="h-[20px] w-[20px] mx-3" alt="Logo"/>
-    <h1 className='text-slate-500 font-mainfont font-normal cursor-pointer  hover:text-black hover:font-bold'>Product List</h1>
+    <div className='LIST-Router flex mt-3'>
+    <img src="../icons/box.png" className="h-[20px] w-[20px] ml-3 md:mx-3" alt="Logo"/>
+    <h1 className='text-slate-500 font-mainfont font-normal cursor-pointer text-xs md:text-lg hover:text-black hover:font-bold'>Product List</h1>
     </div>
 
 </div>
